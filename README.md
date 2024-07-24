4444d505d930e598ec8dd77f05aec79d7b5234ed
Max Schmitt <max@schmitt.mx>
2024-07-24 11:18:03 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+```
+npm ci
+node test.js
+```
---
+++ test.js
@@ -0,0 +1,22 @@
+const socks = require('socksv5');
+const http2 = require('http2');
+const fs = require('fs');
+
+const server = socks.createServer((info, accept, deny) => accept());
+
+server.listen(1080, '127.0.0.1', function() {
+  console.log('SOCKS server listening on port 1080');
+});
+server.useAuth(socks.auth.None());
+
+const http2Server = http2.createSecureServer({
+  key: fs.readFileSync('server_key.pem'),
+  cert: fs.readFileSync('server_cert.pem'),
+}, (req, res) => {
+  res.end('Hello, World!');
+});
+
+http2Server.listen(1081, '127.0.0.1', () => {
+  console.log('HTTP2 server listening on port 443');
+  console.log(`curl -v -k --proxy socks5h://127.0.0.1:1080 https://127.0.0.1:1081`);
+});
