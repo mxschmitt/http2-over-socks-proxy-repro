d2088fc45dd4ef12491523207a1ae43acd1753f6
Max Schmitt <max@schmitt.mx>
2024-07-24 11:31:23 +0200

    chore: add repro steps

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
+Repro steps:
+
+1. 
 ```
 npm ci
 node test.js
+http_proxy=socks5://127.0.0.1:1080 epiphany-browser
 ```
+
+2. Navigate to https://127.0.0.1:1081
+
+Expected: Works
+Actual: Peer sent fatal TLS alert: No supported application protocol could be negotiated
--- test.js
+++ test.js
@@ -2,7 +2,10 @@ const socks = require('socksv5');
 const http2 = require('http2');
 const fs = require('fs');
 
-const server = socks.createServer((info, accept, deny) => accept());
+const server = socks.createServer((info, accept, deny) => {
+  console.log('SOCKS connection requested');
+  accept()
+});
 
 server.listen(1080, '127.0.0.1', function() {
   console.log('SOCKS server listening on port 1080');
@@ -13,10 +16,11 @@ const http2Server = http2.createSecureServer({
   key: fs.readFileSync('server_key.pem'),
   cert: fs.readFileSync('server_cert.pem'),
 }, (req, res) => {
+  console.log('HTTP2 request received');
   res.end('Hello, World!');
 });
 
 http2Server.listen(1081, '127.0.0.1', () => {
   console.log('HTTP2 server listening on port 443');
-  console.log(`curl -v -k --proxy socks5h://127.0.0.1:1080 https://127.0.0.1:1081`);
+  console.log(`curl -v -k --proxy socks5://127.0.0.1:1080 https://127.0.0.1:1081`);
 });
